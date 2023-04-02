4f2768a63efb7db2912bb548081e0d3da8e46084
Nahid Estes <44173168+NahidEstes@users.noreply.github.com>
2023-04-02 15:42:30 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { BeakerIcon } from "@heroicons/react/24/solid";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>E-commerce project</h1>
+    </div>
+  );
+}
+
+export default App;
